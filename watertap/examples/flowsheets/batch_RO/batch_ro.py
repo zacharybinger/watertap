53ed0173cc2b7a4453eece34f4c2cd2b54afab1e
zacharybinger <zbinger@nrel.gov>
2024-07-31 15:44:31 -0600

    v1.0

--- watertap/examples/flowsheets/batch_RO/batch_ro.py
+++ watertap/examples/flowsheets/batch_RO/batch_ro.py
@@ -7,9 +7,10 @@ from pyomo.environ import (
     Var,
     NonNegativeReals,
     assert_optimal_termination,
+    Objective,
     units as pyunits,
 )
-from idaes.core import FlowsheetBlock
+from idaes.core import FlowsheetBlock, UnitModelCostingBlock
 from watertap.property_models.seawater_prop_pack import SeawaterParameterBlock
 from idaes.core.util.scaling import calculate_scaling_factors, set_scaling_factor
 from watertap.unit_models.reverse_osmosis_1D import (ReverseOsmosis1D, ConcentrationPolarizationType, 
@@ -26,22 +27,37 @@ from idaes.models.unit_models.mixer import (
     MomentumMixingType,
     MaterialBalanceType,
 )
+from watertap.costing import (
+    WaterTAPCosting,
+    PumpType,
+    MixerType,
+    ROType,
+)
 import idaes.core.util.scaling as iscale
 from idaes.core.util.initialization import propagate_state
 from pyomo.environ import TransformationFactory
 from pyomo.network import Arc
 from watertap.unit_models.pressure_changer import Pump
 
-def build():
+def build_system(time_blk=None):
+
     m = ConcreteModel()
     m.fs = FlowsheetBlock(dynamic=False)
+    m.fs.costing = WaterTAPCosting()
     m.fs.properties = SeawaterParameterBlock()
     m.fs.feed = Feed(property_package=m.fs.properties)
+    m.fs.recirc = Feed(property_package=m.fs.properties)
     m.fs.product = Product(property_package=m.fs.properties)
-    # m.fs.disposal = Product(property_package=m.fs.properties)
+    m.fs.disposal = Product(property_package=m.fs.properties)
 
     m.fs.P1 = Pump(property_package=m.fs.properties)
+    m.fs.P1.costing = UnitModelCostingBlock(
+        flowsheet_costing_block=m.fs.costing,
+    )
     m.fs.P2 = Pump(property_package=m.fs.properties)
+    m.fs.P2.costing = UnitModelCostingBlock(
+        flowsheet_costing_block=m.fs.costing,
+    )
 
     m.fs.M1 = Mixer(
         property_package=m.fs.properties,
@@ -58,15 +74,23 @@ def build():
     concentration_polarization_type=ConcentrationPolarizationType.calculated,
     has_full_reporting = True,
     )
+    m.fs.RO.costing = UnitModelCostingBlock(flowsheet_costing_block=m.fs.costing)
 
     # connect unit models
     m.fs.feed_to_P1 = Arc(source=m.fs.feed.outlet, destination=m.fs.P1.inlet)
+    m.fs.recirc_to_P2 = Arc(source=m.fs.recirc.outlet, destination=m.fs.P2.inlet)
     m.fs.P1_to_M1 = Arc(source=m.fs.P1.outlet, destination=m.fs.M1.inlet_1)
+    m.fs.P2_to_M1 = Arc(source=m.fs.P2.outlet, destination=m.fs.M1.inlet_2)
     m.fs.M1_to_RO = Arc(source=m.fs.M1.outlet, destination=m.fs.RO.inlet)
 
-    m.fs.RO_to_product = Arc(source=m.fs.RO.permeate, destination=m.fs.product.inlet)
-    m.fs.RO_to_P2 = Arc(source=m.fs.RO.retentate, destination=m.fs.P2.inlet)
-    m.fs.P2_to_M1 = Arc(source=m.fs.P2.outlet, destination=m.fs.M1.inlet_2)
+    # m.fs.feed_to_P1 = Arc(source=m.fs.feed.outlet, destination=m.fs.P1.inlet)
+    # m.fs.P1_to_M1 = Arc(source=m.fs.P1.outlet, destination=m.fs.M1.inlet_1)
+    # m.fs.M1_to_RO = Arc(source=m.fs.M1.outlet, destination=m.fs.RO.inlet)
+
+    m.fs.RO_permeate_to_product = Arc(source=m.fs.RO.permeate, destination=m.fs.product.inlet)
+    m.fs.RO_retentate_to_disposal = Arc(source=m.fs.RO.retentate, destination=m.fs.disposal.inlet)
+    # m.fs.RO_to_P2 = Arc(source=m.fs.RO.retentate, destination=m.fs.P2.inlet)
+    # m.fs.P2_to_M1 = Arc(source=m.fs.P2.outlet, destination=m.fs.M1.inlet_2)
     TransformationFactory("network.expand_arcs").apply_to(m)
 
     m.fs.properties.set_default_scaling("flow_mass_phase_comp", 1, index=("Liq", "H2O"))
@@ -82,6 +106,10 @@ def build():
 
     m.fs.feed.properties[0].flow_vol_phase["Liq"]
     m.fs.feed.properties[0].mass_frac_phase_comp["Liq", "TDS"]
+    m.fs.recirc.properties[0].flow_vol_phase["Liq"]
+    m.fs.recirc.properties[0].mass_frac_phase_comp["Liq", "TDS"]
+    m.fs.M1.mixed_state[0].flow_vol_phase["Liq"]
+    m.fs.M1.mixed_state[0].conc_mass_phase_comp["Liq", "TDS"]
 
     return m
 
@@ -111,6 +139,22 @@ def set_operating_conditions(m, Qin = 1, Cin = 35):
         units=pyunits.kg / pyunits.s,
         doc="System Feed Flowrate",
     )
+    
+    # m.fs.feed_salinity = Var(
+    #     initialize=35,
+    #     bounds=(0, 2000),
+    #     domain=NonNegativeReals,
+    #     units=pyunits.dimensionless,
+    #     doc="System Water Recovery",
+    # )
+
+    # m.fs.feed_flow_mass = Var(
+    #     initialize=1,
+    #     bounds=(0.00001, 1e6),
+    #     domain=NonNegativeReals,
+    #     units=pyunits.kg / pyunits.s,
+    #     doc="System Feed Flowrate",
+    # )
 
     iscale.set_scaling_factor(m.fs.water_recovery, 10)
     iscale.set_scaling_factor(m.fs.feed_flow_mass, 1)
@@ -133,6 +177,11 @@ def set_operating_conditions(m, Qin = 1, Cin = 35):
 
     m.fs.feed.properties[0].pressure.fix(101325)                           # pressure (Pa)
     m.fs.feed.properties[0].temperature.fix(273.15 + 25)                   # temperature (K)
+
+    m.fs.recirc.properties[0].flow_mass_phase_comp["Liq", "H2O"].fix(0.1)
+    m.fs.recirc.properties[0].flow_mass_phase_comp["Liq", "TDS"].fix(0.010275)
+    m.fs.recirc.properties[0].pressure.fix(101325)                           # pressure (Pa)
+    m.fs.recirc.properties[0].temperature.fix(273.15 + 25)                   # temperature (K)
 
     # high pressure pump, 2 degrees of freedom
     m.fs.P1.efficiency_pump.fix(0.80)                                    # pump efficiency (-)
@@ -168,6 +217,16 @@ def set_operating_constraints(m):
     )
 
 
+def add_costing(m):
+
+    m.fs.costing.cost_process()
+    product_flow_vol_total = m.fs.product.properties[0].flow_vol
+    m.fs.costing.add_annual_water_production(product_flow_vol_total)
+    m.fs.costing.add_specific_energy_consumption(product_flow_vol_total)
+    m.fs.costing.add_LCOW(product_flow_vol_total)
+    # m.fs.lcow_objective = Objective(expr=m.fs.costing.LCOW)
+
+
 def initialize_mixer(m, guess = True):
     if guess:
         recovery_guess = 0.3
@@ -183,21 +242,32 @@ def do_forward_initialization_pass(m, pass_num=1):
     m.fs.feed.initialize()
     propagate_state(m.fs.feed_to_P1)
 
+    m.fs.recirc.initialize()
+    propagate_state(m.fs.recirc_to_P2)
+
     m.fs.P1.initialize()
     propagate_state(m.fs.P1_to_M1)
 
-    if pass_num > 0:
-        m.fs.M1.initialize()
-    else:
-        initialize_mixer(m)
+    m.fs.P2.initialize()
+    propagate_state(m.fs.P2_to_M1)
+
+    # if pass_num > 0:
+    #     m.fs.M1.initialize()
+    # else:
+    #     initialize_mixer(m)
+    m.fs.M1.initialize()
     propagate_state(m.fs.M1_to_RO)
 
     m.fs.RO.initialize()
-    propagate_state(m.fs.RO_to_product)
-    propagate_state(m.fs.RO_to_P2)
+    propagate_state(m.fs.RO_permeate_to_product)
+    propagate_state(m.fs.RO_retentate_to_disposal)
 
-    m.fs.P2.initialize()
-    propagate_state(m.fs.P2_to_M1)
+    m.fs.product.initialize()
+    m.fs.disposal.initialize()
+    # propagate_state(m.fs.RO_to_P2)
+
+    # m.fs.P2.initialize()
+    # propagate_state(m.fs.P2_to_M1)
 
 
 def initialize(m):
@@ -208,7 +278,7 @@ def initialize(m):
         print_results(m)
 
 
-def optimize(m, Q_ro = 0.965):
+def optimize(m, water_recovery=0.1, Q_ro = 0.965):
 
     m.fs.feed_flow_mass.unfix()
     m.fs.RO.inlet.flow_mass_phase_comp[0,"Liq", "H2O"].fix(Q_ro)
@@ -252,25 +322,34 @@ def print_results(m):
     print(f'MIXER INLET 1: {value(m.fs.M1.inlet_1_state[0].flow_mass_phase_comp["Liq", "H2O"]):<5.2f}')
     print(f'MIXER INLET 2: {value(m.fs.M1.inlet_2_state[0].flow_mass_phase_comp["Liq", "H2O"]):<5.2f}')
     print(f'MIXER OUTLET: {value(m.fs.M1.mixed_state[0].flow_mass_phase_comp["Liq", "H2O"]):<5.2f}')
+    print(f'MIXER CONC: {value(pyunits.convert(m.fs.M1.mixed_state[0].conc_mass_phase_comp["Liq", "TDS"], to_units=pyunits.g/pyunits.L)):<5.2f} {pyunits.get_units(pyunits.convert(m.fs.M1.mixed_state[0].conc_mass_phase_comp["Liq", "TDS"], to_units=pyunits.g/pyunits.L))}')
     print('\n')
     print(f'PUMP 1 INLET: {value(m.fs.P1.control_volume.properties_in[0.0].flow_mass_phase_comp["Liq", "H2O"]):<5.2f}')
     print(f'PUMP 1 OUTLET: {value(m.fs.P1.control_volume.properties_out[0.0].flow_mass_phase_comp["Liq", "H2O"]):<5.2f}')
-    print(f'PUMP PRESSURE: {value(pyunits.convert(m.fs.P1.control_volume.properties_out[0.0].pressure, to_units=pyunits.bar)):<5.2f}')
+    print(f'PUMP 1 PRESSURE: {value(pyunits.convert(m.fs.P1.control_volume.properties_out[0.0].pressure, to_units=pyunits.bar)):<5.2f}')
+    print('\n')
+    print(f'PUMP 2 INLET: {value(m.fs.P2.control_volume.properties_in[0.0].flow_mass_phase_comp["Liq", "H2O"]):<5.2f}')
+    print(f'PUMP 2 OUTLET: {value(m.fs.P2.control_volume.properties_out[0.0].flow_mass_phase_comp["Liq", "H2O"]):<5.2f}')
+    print(f'PUMP 2 PRESSURE: {value(pyunits.convert(m.fs.P2.control_volume.properties_out[0.0].pressure, to_units=pyunits.bar)):<5.2f}')
     print('\n')
     print(f'RO FEED: {value(m.fs.RO.inlet.flow_mass_phase_comp[0,"Liq", "H2O"]):<5.2f}')
     print(f'RO PRODUCT: {value(m.fs.RO.permeate.flow_mass_phase_comp[0,"Liq", "H2O"]):<5.2f}')
     print(f'RO BRINE: {value(m.fs.RO.retentate.flow_mass_phase_comp[0,"Liq", "H2O"]):<5.2f}')
+    print('\n')
+    print(f'LCOW: {value(m.fs.costing.LCOW):<5.2f} {pyunits.get_units(m.fs.costing.LCOW)}')
     print('\n\n')
     print(m.fs.M1.report())
     print(m.fs.RO.report())
 
 
 def main():
-    m = build()
+    m = build_system()
     set_operating_conditions(m)
+    add_costing(m)
     initialize(m)
     optimize(m)
     solve(m)
+    print_results(m)
     
 
 if __name__ == "__main__":
--- watertap/examples/flowsheets/batch_RO/batch_ro_class.py
+++ watertap/examples/flowsheets/batch_RO/batch_ro_class.py
@@ -3,28 +3,22 @@ import os
 import numpy as np
 import pandas as pd
 import logging
-from collections import deque
 from os.path import join, dirname
 # Pyomo imports
 from pyomo.environ import Set, Expression, value, Objective
-import datetime
 # IDAES imports
 from idaes.apps.grid_integration.multiperiod.multiperiod import MultiPeriodModel
 from idaes.core.solvers.get_solver import get_solver
 import idaes.logger as idaeslog
 # Flowsheet function imports
-from analysisWaterTAP.flowsheets.FFR_RO.multiperiod.ffrro_config_mp import (
-    fix_dof_and_initialize,
-)
-import matplotlib.pyplot as plt
-import matplotlib.ticker as mtick
-from matplotlib.ticker import PercentFormatter
-import matplotlib.dates as mdates
-# import seaborn as sns
-from idaes.core.surrogate.pysmo_surrogate import PysmoRBFTrainer, PysmoSurrogate
+
 from pyomo.environ import Var, value, units as pyunits
-from analysisWaterTAP.flowsheets.FFR_RO.ffrro_system import *
+from watertap.examples.flowsheets.batch_RO.batch_ro import *
+from watertap.examples.flowsheets.batch_RO.batch_ro_config import *
+
 solver = get_solver()
+
+
 def get_variable_pairs(t1, t2):
     """
     This function returns pairs of variables that need to be connected across two time periods
@@ -36,88 +30,53 @@ def get_variable_pairs(t1, t2):
     Returns:
         None
     """
-    return []
+    # return []
+    return [
+        (t2.fs.recirc.properties[0].flow_mass_phase_comp["Liq", "H2O"], t1.fs.disposal.properties[0].flow_mass_phase_comp["Liq", "H2O"]),
+        (t2.fs.recirc.properties[0].flow_mass_phase_comp["Liq", "TDS"], t1.fs.disposal.properties[0].flow_mass_phase_comp["Liq", "TDS"]),
+        (t2.fs.recirc.properties[0].pressure, t1.fs.disposal.properties[0].pressure),
+        (t2.fs.recirc.properties[0].temperature, t1.fs.disposal.properties[0].temperature),
+    ]
 
 def unfix_dof(
         m,
         water_recovery=None,
-        fixed_pressure=None,
-        Qin = None,
-        Qout = None,
-        Qprod=None,
-        mem_width = None,
-        mem_length = None,
-        velocity = None,
-        objective=None):
+        Q_ro=None,
+        time_idx = 0):
     """
     This function unfixes a few degrees of freedom for optimization
 
     Args:
-        feed_flow_rate: 
+        water_recovery: 
+        Q_ro:
 
     Returns:
         None
     """
 
-    if objective == 'LCOW':
-        m.fs.lcow_objective = Objective(expr=m.fs.costing.LCOW)
+    optimize(m, water_recovery=water_recovery, Q_ro = Q_ro)
 
-    print("\n\nDOF before optimization: ", degrees_of_freedom(m))
-    if water_recovery is not None:
-        print(f"\n------- Fixed Recovery at {100*water_recovery}% -------")
-        # product mass flow rate fraction of feed [-]
-        m.fs.water_recovery.fix(water_recovery)
-        m.fs.feed_flow_mass.fix(Qprod/water_recovery)
-        print(f"------- Fixed Feed Flow Rate at {Qprod/water_recovery:<3.1f} kg/s -------")
-    else:
-        m.fs.water_recovery.unfix()
-        m.fs.water_recovery.setlb(0.01)
-        m.fs.water_recovery.setub(0.99)
+    if time_idx > 0:
+        m.fs.recirc.properties[0].flow_mass_phase_comp["Liq", "H2O"].unfix()
+        m.fs.recirc.properties[0].flow_mass_phase_comp["Liq", "TDS"].unfix()
+        m.fs.recirc.properties[0].pressure.unfix()
+        m.fs.recirc.properties[0].temperature.unfix()
 
-    if Qprod is not None:
-        print(f"------- Fixed Product Flow at {Qprod} kg/s -------")
-        m.fs.perm_flow_mass.fix(Qprod)
-        m.fs.feed_flow_mass.unfix()
 
-    if fixed_pressure is not None:
-        print(f"\n------- Fixed Pressure at {fixed_pressure} -------\n")
-        m.fs.P1.control_volume.properties_out[0].pressure.fix(fixed_pressure)
-    else:
-        print(f"------- Unfixed Pressure -------")
-        m.fs.P1.control_volume.properties_out[0].pressure.unfix()
-        m.fs.P2.control_volume.properties_out[0].pressure.unfix()
-
-def add_costing_constraints(mp):
-    n_time_points = len(mp.blocks)
-
-    # Total cost
-    @mp.Expression(doc="total cost")
-    def adj_LCOW(b):
-        # The annualized capital cost is evenly distributed to the multiperiod
-        return (b.blocks[0].process.fs.costing.LCOW + b.blocks[1].process.fs.costing.LCOW)/2
-    
-    @mp.Expression(doc="total cost")
-    def LCOW(b):
-        return b.adj_LCOW
-    
-    # Set objective
-    mp.obj = Objective(expr=mp.LCOW)
-
-def add_operating_constraints(mp):
-    n_time_points = len(mp.blocks)
-    for t in range(n_time_points):
-        # Set operating constraints
-        # mp.blocks[t].process.fs.P1.control_volume.properties_in[0].pressure.fix(8e5)
-        # mp.blocks[t].process.fs.P2.control_volume.properties_in[0].pressure.fix(8e5)
-        # mp.blocks[t].process.fs.feed_flow_mass.fix(77.77)
-        # mp.blocks[t].process.fs.feed_concentration.fix(0.95)
-        mp.blocks[t].process.fs.water_recovery.fix(mp.water_recovery)
-        mp.blocks[t].process.fs.duty_cycle.fix(mp.duty_cycle)
+# def add_operating_constraints(mp):
+#     n_time_points = len(mp.blocks)
+    # for t in range(n_time_points):
+    #     # Set operating constraints
+    #     # mp.blocks[t].process.fs.P1.control_volume.properties_in[0].pressure.fix(8e5)
+    #     # mp.blocks[t].process.fs.P2.control_volume.properties_in[0].pressure.fix(8e5)
+    #     # mp.blocks[t].process.fs.feed_flow_mass.fix(77.77)
+    #     # mp.blocks[t].process.fs.feed_concentration.fix(0.95)
+    #     mp.blocks[t].process.fs.water_recovery.fix(mp.water_recovery)
+    #     mp.blocks[t].process.fs.duty_cycle.fix(mp.duty_cycle)
 
 def create_multiperiod_ffrro_model(
         n_time_points=2,
-        recovery=0.8,
-        duty_cycle=0.99,
+        recovery=0.1,
 ):
     """
     This function creates a multi-period pv battery flowsheet object. This object contains 
@@ -129,63 +88,47 @@ def create_multiperiod_ffrro_model(
     Returns:
         Object containing multi-period vagmd batch flowsheet model
     """
+    watertap_solver = get_solver()
+
     mp = MultiPeriodModel(
         n_time_points=n_time_points,
         process_model_func=build_system,
         linking_variable_func=get_variable_pairs,
         initialization_func=fix_dof_and_initialize,
         unfix_dof_func=unfix_dof,
+        solver=watertap_solver,
         outlvl=logging.WARNING,
     )
 
-    modes = ['System A', 'System B']
-    flowsheet_options={ t: {"configuration":modes[t],} 
+    # mp.build_multi_period_model()
+
+    # modes = ['System A', 'System B']
+    # flowsheet_options={ t: {"configuration":modes[t],} 
+    #                         for t in range(n_time_points)
+    # }
+
+    flowsheet_options={ t: {"time_blk":t,} 
                             for t in range(n_time_points)
     }
 
     mp.build_multi_period_model(
         model_data_kwargs=flowsheet_options,
-        flowsheet_options=flowsheet_options,
-        initialization_options=None,
-        unfix_dof_options={
-            "water_recovery": recovery,
-            "fixed_pressure":None, 
-            "Qprod":77.77, 
-            "objective":None}
-        )
+        unfix_dof_options={'water_recovery':0.1, 'Q_ro':0.965})
     
-    mp.water_recovery = Var(
-        initialize=0.8,
-        bounds=(0, 0.99),
-        domain=NonNegativeReals,
-        units=pyunits.dimensionless,
-        doc="System Water Recovery",
-    )
-    
-    mp.duty_cycle = Var(
-        initialize=duty_cycle,
-        bounds=(0, 0.99),
-        domain=NonNegativeReals,
-        units=pyunits.dimensionless,
-        doc="3rd Stage Duty Cycle",
-    )
-
-    # @mp.Constraint(doc="Water Recovery")
-    # def eq_water_recovery(b):
-    #     return (b.total_capital_cost / 20)
 
     active_blks = mp.get_active_process_blocks()
+    print(active_blks)
     # Initialize and unfix dof for each period
-    solver = get_solver()
-    for blk in active_blks:
+    # solver = get_solver()
+    for idx, blk in enumerate(active_blks):
         fix_dof_and_initialize(
             m=blk
         )
-        result = solver.solve(blk)
-        unfix_dof(m=blk, water_recovery=value(mp.water_recovery), fixed_pressure=None, Qprod = 77.77, mem_length=7.0)
+        result = solve(blk, solver=watertap_solver, tee=False, raise_on_failure=True)
+        unfix_dof(m=blk, water_recovery=recovery, Q_ro=0.965, time_idx=idx)
     
-    add_costing_constraints(mp)
-    add_operating_constraints(mp)
+    # add_costing_constraints(mp)
+    # add_operating_constraints(mp)
 
     return mp
     
@@ -195,7 +138,8 @@ def solve(model, solver=None, tee=False, raise_on_failure=False):
         solver = get_solver()
 
     print("\n--------- SOLVING ---------\n")
-    add_operating_constraints(model)
+    print(f'Degrees of Freedom: {degrees_of_freedom(model)}')
+    # add_operating_constraints(model)
     results = solver.solve(model, tee=tee)
 
     if check_optimal_termination(results):
@@ -205,27 +149,33 @@ def solve(model, solver=None, tee=False, raise_on_failure=False):
         "The current configuration is infeasible. Please adjust the decision variables."
     )
     if raise_on_failure:
-        debug(model, solver=solver, automate_rescale=False, resolve=False)
-        # debug(model, solver=solver, automate_rescale=False, resolve=False)
-        # check_jac(model)
+        print("\n--------- INFEASIBLE SOLVE!!! ---------\n")
+
+        print("\n--------- CLOSE TO BOUNDS ---------\n")
+        print_close_to_bounds(model)
+
+        print("\n--------- INFEASIBLE BOUNDS ---------\n")
+        print_infeasible_bounds(model)
+
+        print("\n--------- INFEASIBLE CONSTRAINTS ---------\n")
+        print_infeasible_constraints(model)
+
         raise RuntimeError(msg)
     else:
         print(msg)
         # debug(model, solver=solver, automate_rescale=False, resolve=False)
         # check_jac(model)
-        return results
+        assert False
 
 if __name__ == "__main__":
-    mp = create_multiperiod_ffrro_model()
-    mp.water_recovery.fix(0.86)
-    mp.duty_cycle.fix(0.95)
-    results = solve(mp)
+    mp = create_multiperiod_ffrro_model(n_time_points=10)
+    results = solve(mp, raise_on_failure=True)
     print("Solve Complete")
 
-    print(f'\n\n{"Normal Operation:":<30}')
-    display_flow_table(mp.blocks[0].process)
-    # display_system_metrics(mp.blocks[0].process)
-    
-    print(f'\n\n{"Split Flow Operation:":<30}')
-    display_flow_table(mp.blocks[1].process)
-    # display_system_metrics(mp.blocks[1].process)
+    for blk in mp.get_active_process_blocks():
+        print(blk.fs.M1.report())
+
+    ro_feed_salinity = [value(blk.fs.M1.mixed_state[0.0].flow_mass_phase_comp["Liq", "TDS"]) for blk in mp.get_active_process_blocks()]
+    ro_feed_conc = [value(blk.fs.M1.mixed_state[0.0].conc_mass_phase_comp["Liq", "TDS"]) for blk in mp.get_active_process_blocks()]
+    print(ro_feed_salinity)
+    print(ro_feed_conc)
